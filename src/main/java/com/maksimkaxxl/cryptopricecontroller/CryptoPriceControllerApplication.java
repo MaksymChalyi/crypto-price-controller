e3a954c0bd0e7b007c1ea3574963ad0f36324669
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-04-30 17:48:41 +0300

    init

---
+++ src/main/java/com/maksimkaxxl/cryptopricecontroller/CryptoPriceControllerApplication.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.cryptopricecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPriceControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoPriceControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maksimkaxxl/cryptopricecontroller/CryptoPriceControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.cryptopricecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPriceControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
